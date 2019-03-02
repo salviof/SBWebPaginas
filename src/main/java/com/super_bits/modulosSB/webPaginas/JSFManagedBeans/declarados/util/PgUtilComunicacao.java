bebb08d8311a42baab56f1e62586fba3493aa918
Salvio Furbino Elias <salviof@gmail.com>
2019-03-01 22:02:45 -0300

    Atualizacao automática

--- src/main/java/com/super_bits/modulosSB/webPaginas/JSFManagedBeans/declarados/util/PgUtilComunicacao.java
+++ src/main/java/com/super_bits/modulosSB/webPaginas/JSFManagedBeans/declarados/util/PgUtilComunicacao.java
@@ -6,6 +6,7 @@ package com.super_bits.modulosSB.webPaginas.JSFManagedBeans.declarados.util;
 
 import com.super_bits.modulosSB.SBCore.ConfigGeral.SBCore;
 import com.super_bits.modulosSB.SBCore.modulos.comunicacao.ComunicacaoAcaoSistema;
+import com.super_bits.modulosSB.SBCore.modulos.comunicacao.ItfComunicacao;
 import com.super_bits.modulosSB.SBCore.modulos.comunicacao.ItfRespostaComunicacao;
 import com.super_bits.modulosSB.webPaginas.JSFManagedBeans.formularios.interfaces.ItfPaginaAtual;
 import com.super_bits.modulosSB.webPaginas.util.UtilSBWP_JSFTools;
@@ -29,6 +30,7 @@ public class PgUtilComunicacao implements Serializable {
     @Inject
     private ItfPaginaAtual paginaAtual;
     private String codigoComunicacao;
+    private String idTipoResposta;
 
     public void exibirModalComunicacaoCodigo() {
         try {
@@ -41,6 +43,23 @@ public class PgUtilComunicacao implements Serializable {
             SBCore.RelatarErro(FabErro.SOLICITAR_REPARO, "Erro ao exibir comunicação por código ", t);
         }
 
+    }
+
+    public void respostaRapida() {
+        try {
+            if (codigoComunicacao == null) {
+                throw new UnsupportedOperationException("Defina o Código de comunicação para exeção deste método");
+            }
+            if (idTipoResposta == null) {
+                throw new UnsupportedOperationException("Defina o id do tipo de resposta");
+            }
+            ItfComunicacao cm = SBCore.getCentralDeComunicacao().getComnunicacaoRegistrada(codigoComunicacao);
+            ItfRespostaComunicacao resposta = cm.getRepostasPossiveis().stream().filter(resp -> resp.getTipoResposta().getId() == Integer.valueOf(idTipoResposta)).findFirst().get();
+            SBCore.getCentralDeComunicacao().responderComunicacao(cm, resposta);
+            paginaUtil.atualizaTelaPorID("idAreaSBTopoInterface");
+        } catch (Throwable t) {
+            SBCore.RelatarErro(FabErro.SOLICITAR_REPARO, "Erro ao exibir comunicação por código ", t);
+        }
     }
 
     public void responderComunicacao(ComunicacaoAcaoSistema pComunicacao, ItfRespostaComunicacao pResposta) {
@@ -58,6 +77,14 @@ public class PgUtilComunicacao implements Serializable {
     public void metodoRespostaModal(SelectEvent event) {
         //paginaUtil.atualizaTelaPorID("painelMenuUsuario");
         paginaUtil.enviaMensagem("Teste");
+    }
+
+    public String getIdTipoResposta() {
+        return idTipoResposta;
+    }
+
+    public void setIdTipoResposta(String idTipoResposta) {
+        this.idTipoResposta = idTipoResposta;
     }
 
 }
